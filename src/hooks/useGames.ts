42fd82473da5ce4db10c09266393d14aca7586fd
Jonathan Yazdanpanah <75292585+jyazdan@users.noreply.github.com>
2023-04-27 17:29:40 -0400

    filter games by platform

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -12,9 +12,22 @@ export interface Game {
   name: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
-  metacritic: number
+  metacritic: number;
 }
 
-const useGames = (selectedGenre: Genre | null) => useData<Game>('/games', {params: {genres: selectedGenre?.id}}, [selectedGenre?.id])
+const useGames = (
+  selectedGenre: Genre | null,
+  selectedPlatform: Platform | null
+) =>
+  useData<Game>(
+    "/games",
+    {
+      params: {
+        genres: selectedGenre?.id,
+        parent_platforms: selectedPlatform?.id,
+      },
+    },
+    [selectedGenre?.id, selectedPlatform?.id]
+  );
 
 export default useGames;
